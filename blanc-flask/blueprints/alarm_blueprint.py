dc81b5c281e7d6895bb33bc533b6d21f23580253
anuscode <dldyddn0624@gmail.com>
2021-03-25 16:30:53 +0900

    an issue that unavialable alarms when user is 404 fixed

--- blanc-flask/blueprints/alarm_blueprint.py
+++ blanc-flask/blueprints/alarm_blueprint.py
@@ -3,9 +3,11 @@
 import json
 import mongoengine
 
+from flask import current_app as app
 from flask import Blueprint
 from flask import request
 from flask import Response
+
 from model.models import Alarm, User
 
 alarms_blueprint = Blueprint('alarms_blueprint', __name__)
@@ -14,38 +16,42 @@ alarms_blueprint = Blueprint('alarms_blueprint', __name__)
 @alarms_blueprint.route('/alarms', methods=['GET'])
 def list_alarms():
     uid = request.headers.get("uid", None)
-    
+
     user = User.objects.get_or_404(uid=uid)
-    
+
     alarm = Alarm.objects(owner=user).first()
     if not alarm:
         alarm = Alarm(owner=user, records=[]).save()
-    
+
     records = alarm.records
-    
+
     result = []
     for push in records:
         try:
             push_dict: dict = push.as_dict()
             result.append(push_dict)
-        except mongoengine.errors.DoesNotExist:
-            pass
-    
-    return Response(
-        json.dumps(result), mimetype='application/json')
+        except mongoengine.DoesNotExist:
+            nickname = user.nickname
+            user_id = str(user.id)
+            not_found_user_id = push.user_id
+            message = "Not found a user({not_found_user_id}) in alarm of {nickname}({user_id}) ".format(
+                not_found_user_id=not_found_user_id, nickname=nickname, user_id=user_id)
+            app.logger.error(message)
+
+    return Response(json.dumps(result), mimetype='application/json')
 
 
 @alarms_blueprint.route('/alarms', methods=['PUT'])
 def update_all_alarms_as_read():
     uid = request.headers.get("uid", None)
-    
+
     user = User.objects.get_or_404(uid=uid)
-    
+
     alarm = Alarm.objects.get_or_404(owner=user)
     records = alarm.records
-    
+
     for push in records:
         push.is_read = True
-    
+
     alarm.save()
     return Response("", mimetype='application/json')
